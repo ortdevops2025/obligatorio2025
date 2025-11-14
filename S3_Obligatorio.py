5da6cde7d176ae08abf3ad6c8b9c9af707d913fa
marcelososadevops <marcelososadevops@gmail.com>
2025-11-13 21:03:59 -0300

    Correccion path S3

--- S3_Obligatorio.py
+++ S3_Obligatorio.py
@@ -19,16 +19,16 @@ except ClientError as e:
 # Subir los archivos de la aplicacion
 try:
     #CUIDADO CAMBIAR RUTA PARA QUE SEA UNIVERSAL
-    s3.upload_file('/aplicacion/app.css', bucket_name, 'app.css')
-    s3.upload_file('/aplicacion/app.js', bucket_name, 'app.js')
-    s3.upload_file('/aplicacion/config.php', bucket_name, 'config.php')
-    s3.upload_file('/aplicacion/index.html', bucket_name, 'index.html')
-    s3.upload_file('/aplicacion/index.php', bucket_name, 'index.php')
-   # s3.upload_file('/aplicacion/init_db.sql', bucket_name, 'init_db.sql')
-    s3.upload_file('/aplicacion/login.css', bucket_name, 'login.css')
-    s3.upload_file('/aplicacion/login.html', bucket_name, 'login.html')
-    s3.upload_file('/aplicacion/login.js', bucket_name, 'login.js')
-    s3.upload_file('/aplicacion/login.php', bucket_name, 'login.php')
+    s3.upload_file('aplicacion/app.css', bucket_name, 'app.css')
+    s3.upload_file('aplicacion/app.js', bucket_name, 'app.js')
+    s3.upload_file('aplicacion/config.php', bucket_name, 'config.php')
+    s3.upload_file('aplicacion/index.html', bucket_name, 'index.html')
+    s3.upload_file('aplicacion/index.php', bucket_name, 'index.php')
+    s3.upload_file('aplicacion/init_db.sql', bucket_name, 'init_db.sql')
+    s3.upload_file('aplicacion/login.css', bucket_name, 'login.css')
+    s3.upload_file('aplicacion/login.html', bucket_name, 'login.html')
+    s3.upload_file('aplicacion/login.js', bucket_name, 'login.js')
+    s3.upload_file('aplicacion/login.php', bucket_name, 'login.php')
  #   s3.upload_file('/aplicacion/README.md', bucket_name, 'README.md')
 
     print("Archivos subidos correctamente.")
